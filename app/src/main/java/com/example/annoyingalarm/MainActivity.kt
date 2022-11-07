d1a56d21922675a7611b7a6784aba021dc754424
SDAxium <38741548+SDAxium@users.noreply.github.com>
2022-11-06 19:45:24 -0500

    Card game (#13)
    * Card Game Initial
    
    Created a basic card matching game that sends the user back to a base fragment on completion
    
    * updated dummy text

--- app/src/main/java/com/example/annoyingalarm/MainActivity.kt
+++ app/src/main/java/com/example/annoyingalarm/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.annoyingalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val matchingGameFragment: Fragment = MatchingGameFragment()
+        val alarmFragment: Fragment = AlarmFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item->
+            lateinit var fragment: Fragment
+            when(item.itemId)
+            {
+                R.id.matchingGame -> fragment = matchingGameFragment
+                R.id.alarm -> fragment = alarmFragment
+            }
+            replaceFragment(fragment)
+            true
+        }
+        bottomNavigationView.selectedItemId = R.id.alarm
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.alarm_frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
